800bc9ef0834f688efa32f78d6459177609c0a02
figueroaf <figueroaf@moravian.edu>
2023-12-29 09:57:47 -0500

    Simple Todo list not done yet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'; // Import the UUID library
 
 function App() {
+  const [newItem, setNewItem] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos(() => {
+      return [
+        ...todos,
+        { id: uuidv4(), title: newItem, completed: false }, // Use uuidv4() to generate a unique ID
+      ];
+    });
+    setNewItem('');
+  }
+
+  function toggleTodo (id, completed) {
+    setTodos(() => {
+      return todos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo (id) {
+    setTodos(() => {
+      return todos.filter(todo => todo.id !== id)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className='body-background'>
+    <div className='wrapper'>
+      <div className="App">
+        <form onSubmit={handleSubmit} className='new-item-form'>
+          <div className='form-row'>
+            <label htmlFor='new-item-input'>New Item</label>
+            <input
+              value={newItem}
+              onChange={(e) => setNewItem(e.target.value)}
+              type='text'
+              id='item'
+            />
+          </div>
+          <button className="btn">Add</button>
+        </form>
+      </div>
+     <div className='Todo'>
+     <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        {todos.length === 0 && <li className='list-item'>No items yet!</li>}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input checked={todo.completed} onChange={ e => toggleTodo(todo.id,e.target.checked)} type='checkbox' />
+                {todo.title}
+              </label>
+              <button onClick={() => deleteTodo(todo.id)} className='btn'>Delete</button>
+            </li>
+          );
+        })}
+      </ul>
+     </div>
+     </div>
+     </div>
+    </>
   );
 }
 
